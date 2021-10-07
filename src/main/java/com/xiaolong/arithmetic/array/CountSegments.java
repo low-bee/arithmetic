a6bb8e90a6c46bc39ea8596981528c0f438b9ace
xiaolong <xiaolongorigin@gmail.com>
2021-10-08 07:27:23 +0800

    两个算法

--- src/main/java/com/xiaolong/arithmetic/array/CountSegments.java
+++ src/main/java/com/xiaolong/arithmetic/array/CountSegments.java
@@ -1,6 +1,20 @@
-package com.xiaolong.arithmetic.array;/** 
- * @Description: TODO(这里用一句话描述这个类的作用) 
+package com.xiaolong.arithmetic.array;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+/**
+ * @Description:
+ * 统计字符串中的单词个数，这里的单词指的是连续的不是空格的字符。
+ *
+ * 请注意，你可以假定字符串里不包括任何不可打印的字符
  * @Author xiaolong
- * @Date 2021/10/7 6:42 上午 
- */public class CountSegments {
+ * @Date 2021/10/7 6:42 上午
+ */
+public class CountSegments {
+    public int countSegments(String s) {
+        return (int) Arrays.stream(s.split(" ")).filter(a -> !("".equals(a))).filter( a -> !" ".equals(a)).count();
+    }
 }
--- src/main/java/com/xiaolong/arithmetic/array/RepeatedDnaSequences.java
+++ src/main/java/com/xiaolong/arithmetic/array/RepeatedDnaSequences.java
@@ -1,6 +1,54 @@
-package com.xiaolong.arithmetic.array;/** 
- * @Description: TODO(这里用一句话描述这个类的作用) 
+package com.xiaolong.arithmetic.array;
+
+import java.util.*;
+
+/**
+ * @Description:
+ * 所有 DNA 都由一系列缩写为 'A'，'C'，'G' 和 'T' 的核苷酸组成，例如："ACGAATTCCG"。在研究 DNA 时，识别 DNA 中的重复序列有时会对研究非常有帮助。
+ *
+ * 编写一个函数来找出所有目标子串，目标子串的长度为 10，且在 DNA 字符串 s 中出现次数超过一次。
+ *
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/repeated-dna-sequences
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  * @Author xiaolong
- * @Date 2021/10/8 6:34 上午 
- */public class RepeatedDnaSequences {
+ * @Date 2021/10/8 6:34 上午
+ */
+public class RepeatedDnaSequences {
+
+    static final int L = 10;
+    Map<Character, Integer> bin = new HashMap<Character, Integer>() {{
+        put('A', 0);
+        put('C', 1);
+        put('G', 2);
+        put('T', 3);
+    }};
+
+    public List<String> findRepeatedDnaSequences(String s) {
+        HashSet<String> ans = new HashSet<>();
+        int n = s.length();
+        if (n <= L) {
+            return new ArrayList<>();
+        }
+        int x = 0;
+        for (int i = 0; i < L - 1; ++i) {
+            x = (x << 2) | bin.get(s.charAt(i));
+        }
+        Set<Integer> cnt = new HashSet<>();
+        for (int i = 0; i <= n - L; ++i) {
+            x = ((x << 2) | bin.get(s.charAt(i + L - 1))) & ((1 << (L * 2)) - 1);
+            if (cnt.contains(x)) {
+                ans.add(s.substring(i, i + L));
+            }
+            cnt.add(x);
+        }
+        return new ArrayList<>(ans);
+
+    }
+
+    public static void main(String[] args) {
+        new RepeatedDnaSequences().findRepeatedDnaSequences("AAAAAAAAAAA");
+    }
 }
